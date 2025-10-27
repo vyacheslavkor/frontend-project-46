d5d229f48b807b08132af835a7f274690817bf0b
Vyacheslav Korolev <vkorolev@cs-cart.com>
2025-10-27 22:53:33 +0400

    Small fixed.

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,15 @@ import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
+import pluginJest from 'eslint-plugin-jest'
 
 export default defineConfig([
   stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
   },
 ])
