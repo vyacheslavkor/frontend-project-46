09468065bfb22fa070721294a403cdfac382cf9f
Vyacheslav Korolev <vkorolev@cs-cart.com>
2025-10-27 23:56:08 +0400

    Default value added.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { parse } from './parsers.js'
 import genDiff from './differ.js'
 
-const gendiff = (filepath1, filepath2, format) => {
+const gendiff = (filepath1, filepath2, format = 'stylish') => {
   const parsedData = parse(filepath1, filepath2)
 
   return genDiff(parsedData.firstFile, parsedData.secondFile, format)
